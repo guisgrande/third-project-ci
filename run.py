7dd6dd6d8d5f457c265e04b5465f38bd651320f6
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-04-05 08:42:05 +0000

    Add main methods names

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Displays the name of the game, description and rules, and the option to play or close the program.
+def main_menu():
+    pass
+
+# Close the program
+def quit_program():
+    pass
+
+# Defines who will start the game, through the choice of heads or tails.
+def coin_toss():
+    pass
+
+# Shuffle the cards, deal 4 cards to each player, and turn over the first card. 
+def start_game():
+    pass
+
+# Stops the game and returns to the main menu.
+def stop_game():
+    pass
+
+# Receives the player's decision of which card to discard, and moves it to the discard deck (face revealed).
+def player_discard_action():
+    pass
+
+# Receives the player's decision of which card to take, and moves the card to players hand.
+def player_take_action():
+    pass
+
+# Check the cards in hand and purchase options and make a decision.
+def computer_action():
+    pass
+
+# Check the cards in the hand
+def win_check():
+    pass
+
+# Checks if you have three of a kind in your hand, does not consider suits, only value.
+def extra_round_check():
+    pass
